c7615008828ed46b8bfb73f11a8ba3cc96474869
Bryce Klinker <bryce.klinker@gmail.com>
2018-02-07 06:24:26 -0600

    Added customer list and customer list item to customers root

--- angular-app/src/app/customers/components/customers-root/customers-root.component.spec.ts
+++ angular-app/src/app/customers/components/customers-root/customers-root.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomersRootComponent } from './customers-root.component';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {MatListModule} from "@angular/material";
 
 describe('CustomersRootComponent', () => {
   let httpTestingController: HttpTestingController;
@@ -12,7 +13,10 @@ describe('CustomersRootComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [ CustomersRootComponent ],
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        MatListModule
+      ]
     })
     .compileComponents();
   }));
@@ -28,8 +32,21 @@ describe('CustomersRootComponent', () => {
     fixture.detectChanges();
     const req = httpTestingController.expectOne('http://localhost:5000/api/customers');
     expect(req.request.method).toBe('GET');
-    expect(req.flush([]));
+    expect(req.flush({items: [] }));
   });
+
+  it('should show customers list', async(() => {
+    fixture.detectChanges();
+
+    const req = httpTestingController.expectOne('http://localhost:5000/api/customers');
+    req.flush({items: [{}, {}] });
+
+    fixture.detectChanges(); // IMPORTANT
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('.customers-list').length).toBe(1);
+      expect(fixture.nativeElement.querySelectorAll('.customer-list-item').length).toBe(2);
+    })
+  }))
 
   afterEach(() => {
     httpTestingController.verify();
--- angular-app/src/app/customers/components/customers-root/customers-root.component.ts
+++ angular-app/src/app/customers/components/customers-root/customers-root.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Customer} from "../../../../../../backend/src/api/customers/customer";
+import {ResultList} from "../../../../../../backend/src/api/general/models/result-list";
 
 @Component({
   selector: 'app-customers-root',
@@ -7,12 +9,13 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./customers-root.component.css']
 })
 export class CustomersRootComponent implements OnInit {
+  customers: Customer[];
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.httpClient.get('http://localhost:5000/api/customers')
-      .subscribe(() => {});
+    this.httpClient.get<ResultList<Customer>>('http://localhost:5000/api/customers')
+      .subscribe(result => this.customers = result.items);
   }
 
 }
