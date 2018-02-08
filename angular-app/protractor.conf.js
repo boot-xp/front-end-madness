a9168bf75bda3b8e5be0fd0be9ca864df05595b2
Bryce Klinker <bryce.klinker@gmail.com>
2018-02-08 06:32:44 -0600

    Added starting backend to angular e2e tests

--- angular-app/protractor.conf.js
+++ angular-app/protractor.conf.js
@@ -1,8 +1,11 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+const path = require('path');
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { spawn } = require('child_process');
 
+let backendProcess;
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
@@ -24,5 +27,29 @@ exports.config = {
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+
+
+    return startBackend();
+  },
+  onComplete() {
+    if (!backendProcess && !backendProcess.killed)
+      return;
+
+    backendProcess.kill('SIGTERM');
   }
 };
+
+function startBackend() {
+  return new Promise((resolve, reject) => {
+    backendProcess = spawn('npm', ['start'], { cwd: path.resolve(__dirname, '..', 'backend'), shell: true });
+    backendProcess.stdout.on('data', data => {
+      if (data.indexOf('Now listening'))
+        resolve();
+
+      console.log(`BACKEND: ${data}`)
+    });
+    backendProcess.stderr.on('data', data => console.error(`BACKEND: ${data}`));
+    backendProcess.on('close', code => console.log(`BACKEND: Exited with code ${code}`));
+  })
+
+}
